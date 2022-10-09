83dd873113bb0820854241d45feefafdca0755ba
SvetlanaVasilek <vasilek378@gmail.com>
2022-10-10 00:28:19 +0300

    second task

--- src/test/java/ru/netology/CheckCardNegativeTest.java
+++ src/test/java/ru/netology/CheckCardNegativeTest.java
@@ -1,10 +1,8 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,7 +13,6 @@ public class CheckCardNegativeTest {
 
         @BeforeAll
         static void setUpAll() {
-            //  System.setProperty("webdriver.chrome.driver", "driver/chromedriver"); как на лекции
             WebDriverManager.chromedriver().setup();
         }
 
@@ -35,9 +32,41 @@ public class CheckCardNegativeTest {
         }
 
         @Test
-        void shouldCheck2() {
+        void shouldCheckCardNegative1() {
             driver.get("http://localhost:9999/");
-
+            driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov Ivan"); // ФИО на латинице
+            driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79251234567");
+            driver.findElement(By.className("checkbox")).click();
+            driver.findElement(By.tagName("button")).click();
+            String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+            String actual = driver.findElement(By.className("input__sub")).getText();
+            Assertions.assertEquals(expected, actual);
         }
+    @Test
+    void shouldCheckCardNegative2() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).isDisplayed(); // пропущено ФИО
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79251234567");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldCheckCardNegative3() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("$@#$@#$@#"); // вместо ФИО специсимволы
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79251234567");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
+
+
 
