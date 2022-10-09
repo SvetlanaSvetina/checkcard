bdb1ee3f5fc3073118efbf29b73cfb078ed02839
SvetlanaVasilek <vasilek378@gmail.com>
2022-10-09 22:34:26 +0300

    first task

--- src/test/java/ru/netology/CheckCardNegativeTest.java
+++ src/test/java/ru/netology/CheckCardNegativeTest.java
@@ -1,4 +1,43 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class CheckCardNegativeTest {
-}
+
+        private WebDriver driver;
+
+        @BeforeAll
+        static void setUpAll() {
+            //  System.setProperty("webdriver.chrome.driver", "driver/chromedriver"); как на лекции
+            WebDriverManager.chromedriver().setup();
+        }
+
+        @BeforeEach
+        void setUp() {
+            // driver = new ChromeDriver(); как на лекции
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);
+            driver = new ChromeDriver(options);
+
+        }
+
+        @AfterEach
+        void tearsDown() {
+            driver.quit();
+            driver = null;
+        }
+
+        @Test
+        void shouldCheck2() {
+            driver.get("http://localhost:9999/");
+
+        }
+    }
+
--- src/test/java/ru/netology/CheckCardPositiveTest.java
+++ src/test/java/ru/netology/CheckCardPositiveTest.java
@@ -4,17 +4,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CheckCardPositiveTest {
 
-    private WebDriver driver;
+    private WebDriver driver; // подключаем веб-драйвер
 
     @BeforeAll
     static void setUpAll() {
         //  System.setProperty("webdriver.chrome.driver", "driver/chromedriver"); как на лекции
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup(); // подключаем веб-менеджер драйвер
     }
 
     @BeforeEach
@@ -33,8 +34,15 @@ public class CheckCardPositiveTest {
     }
 
     @Test
-    void shouldCheck1() {
+    void shouldCheckCardPossitive() {
         driver.get("http://localhost:9999/");
-
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван"); // ФИО
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79251234567"); // номер телефона
+        driver.findElement(By.className("checkbox")).click(); // чек-бокс
+        driver.findElement(By.tagName("button")).click(); // кнопка "продолжить"
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        Assertions.assertEquals(expected, actual);
     }
+
 }
